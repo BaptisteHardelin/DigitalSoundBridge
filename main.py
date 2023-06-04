89efeb97a37ca1d55e9f7f5f288e175a0989f814
BaptisteHardelin <baptiste.hardelin@gmail.com>
2023-06-04 23:31:19 +0200

    Add UI + get all USB devices connected

--- main.py
+++ main.py
@@ -5,9 +5,38 @@ tout en jouant le son en direct pendant l'enregistrement.
 from stream_params import StreamParams
 from recorder import Recorder
 import tkinter as tk
+import sounddevice as sd
 
 window = tk.Tk()
 window.title("Sound Bridge")
+
+def get_audio_devices():
+    return sd.query_devices()
+
+def get_default_input_device():
+    return sd.default.device[0]  # Indice du périphérique d'entrée par défaut
+
+def get_default_output_device():
+    return sd.default.device[1]  # Indice du périphérique de sortie par défaut
+
+def display_audio_devices():
+    devices = get_audio_devices()
+    default_input_device = get_default_input_device()
+    default_output_device = get_default_output_device()
+
+    label = tk.Label(window, text="Audio Devices")
+    label.pack()
+
+    for device in devices:
+        device_info = f"{device['index']} "
+        if device['index'] == default_input_device:
+            device_info += "> "
+        elif device['index'] == default_output_device:
+            device_info += "< "
+        device_info += f"{device['name']}, {device['hostapi']} ("
+        device_info += f"{device['max_input_channels']} in, {device['max_output_channels']} out)"
+        device_label = tk.Label(window, text=device_info, justify='left')
+        device_label.pack(anchor='w')
 
 
 if __name__ == "__main__":
@@ -15,6 +44,7 @@ if __name__ == "__main__":
     recorder = Recorder(stream_params)
     recorder.start_recording(99999999999)
 
+    display_audio_devices()
     window.mainloop()
 
     input("Appuyez sur Entrée pour arrêter l'enregistrement...")
